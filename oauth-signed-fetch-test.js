e34665da1597bd5bca2dca509b2e3af3b85c72f0
Jun Yang <jyang825@gmail.com>
2010-10-15 09:52:40 -0700

    Fixed typo.

--- oauth-signed-fetch-test.js
+++ oauth-signed-fetch-test.js
@@ -3,8 +3,8 @@ var oauth = require('./oauth-signed-fetch');
 
 var verifySignedFetch = oauth.verifySignedFetch('http://<public-host-of-this-server>:8081', './igoogle.pem');
 
-http.createServer(function (request, response) {
-  verifySignedFetch(req, res, function() {
+http.createServer(function(request, response) {
+  verifySignedFetch(request, response, function() {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('signed fetch verified\n');
   });
